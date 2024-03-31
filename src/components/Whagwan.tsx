0c42b64b3171c5be6ec510a0d57fa92d3308d358
Arjun Patel <47369740+arjunsamir@users.noreply.github.com>
2024-03-31 16:20:56 -0700

    updated local config

---
+++ src/components/Whagwan.tsx
@@ -0,0 +1,15 @@
+import type React from "react"
+
+console.log("whagwan")
+console.log("fenny")
+console.log("fenny")
+
+const Whagwan: React.FC  = () => {
+  return (
+    <div>
+      <h1>Whagwan</h1>
+    </div>
+  )
+}
+
+export default Whagwan
